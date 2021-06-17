7b36ed4c14e5ab445d0af5f6b972cec88e93ddc1
alvcorcas <alvarocortescasado@gmail.com>
2021-06-17 11:13:46 +0200

    Prego method semi done

--- prego.py
+++ prego.py
@@ -5,5 +5,27 @@ B = Action("B", ['p4'], ['p5'])
 C = Action("C", ['p6'], ['p5'])
 D = Action("D", ['p1', 'p4', 'p5'], ['p3', 'p7'])
 
-acciones = [A, B, C, D]
-print(acciones)
+actions = [A, B, C, D]
+print(actions)
+
+
+def prego(state, targets):
+    global actions
+    result = []
+    for target in targets:
+        if target in state.literals:
+            break
+
+        elif not(target in action.effects for action in actions):
+            result = result + actions
+
+        else:
+            heuristic = actions
+            for action in actions:
+                if target in action.effects:
+                    temp = [action] + prego(state, (precondition for precondition in action.preconditions))
+                    if len(temp) < len(heuristic):
+                        heuristic = temp
+            result = result + heuristic
+    return result
+
