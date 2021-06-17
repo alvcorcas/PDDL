3d7a66099446952da1856af4724b66a2617f788d
Sergio <serrojjim@alum.us,es>
2021-06-17 09:53:52 +0200

    action creado

--- Action.py
+++ Action.py
@@ -5,3 +5,12 @@ Created on Mon Jun 14 15:47:05 2021
 @author: alvar
 """
 
+class Action:
+    def __init__(self, action, preconditions,effects):
+        self.action = action
+        self.preconditions = preconditions
+        self.effects = effects
+
+    def __repr__(self):
+        return self.action
+        
---
+++ prego.py
@@ -0,0 +1,11 @@
+from Action import *
+
+A = Action("A",[],[])
+B = Action("B",[],[])
+C = Action("C",[],[])
+D = Action("D",[],[])
+E = Action("E",[],[])
+
+acciones = [A,B,C,D,E]
+print(acciones)
+
